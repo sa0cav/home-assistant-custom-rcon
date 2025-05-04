45822a746c430b98d0ba60ad9555676f2ba9ecde
sa0cav <92050670+sa0cav@users.noreply.github.com>
2025-05-04 16:50:47 +0200

    Update sensor.py

--- custom_components/rcon/sensor.py
+++ custom_components/rcon/sensor.py
@@ -1,1 +1,24 @@
+from homeassistant.helpers.entity import Entity
+from .const import DOMAIN
 
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
+    add_entities([RconSensor(hass)])
+
+class RconSensor(Entity):
+    def __init__(self, hass):
+        self._hass = hass
+        self._state = None
+
+    @property
+    def name(self):
+        return "RCON Server Status"
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        client: MCRcon = self._hass.data[DOMAIN]["client"]
+        # Exempelkommando, t.ex. "list" för Minecraft
+        resp = client.command("list")
+        self._state = resp
