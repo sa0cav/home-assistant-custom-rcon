2dd6965ed713d2baa8f96972b880dfbeac6f38e4
sa0cav <92050670+sa0cav@users.noreply.github.com>
2025-05-04 16:50:26 +0200

    Update __init__.py

--- custom_components/rcon/__init__.py
+++ custom_components/rcon/__init__.py
@@ -1,1 +1,14 @@
+import asyncio
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+from mcrcon import MCRcon
 
+async def async_setup(hass: HomeAssistant, config: dict):
+    hass.data.setdefault(DOMAIN, {})
+    # Här kan du läsa host/port/password från configuration.yaml eller config_flow
+    host = config[DOMAIN].get("host")
+    port = config[DOMAIN].get("port", 25575)
+    password = config[DOMAIN].get("password")
+    # Spara instansen
+    hass.data[DOMAIN]["client"] = MCRcon(host, password, port)
+    return True
