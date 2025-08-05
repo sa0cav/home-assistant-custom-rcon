462260cf3ac6504bbeef16221ffe44c953ffe771
sa0cav <92050670+sa0cav@users.noreply.github.com>
2025-08-05 22:30:18 +0200

    Update config_flow.py

--- custom_components/rcon/config_flow.py
+++ custom_components/rcon/config_flow.py
@@ -1,8 +1,3 @@
-import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.const import CONF_HOST, CONF_PORT, CONF_PASSWORD, CONF_NAME
-from .const import DOMAIN, GAMES
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_PASSWORD, CONF_NAME
 from .const import DOMAIN, GAMES
@@ -58,30 +53,4 @@ class RconConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(
             title=data[CONF_NAME],
             data=data
-        )
-
-
-class RconConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for RCON integration."""
-
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
-
-    async def async_step_user(self, user_input=None):
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA
-            )
-
-        unique_id = (
-            f"{user_input[CONF_HOST]}_{user_input.get(CONF_PORT,25575)}_"
-            f"{user_input['game']}_{user_input[CONF_NAME]}"
-        )
-        await self.async_set_unique_id(unique_id)
-        self._abort_if_unique_id_configured()
-
-        return self.async_create_entry(
-            title=user_input[CONF_NAME],
-            data=user_input
         )
